97042c3151de68670fe9ced2a6539ed65b19465b
Aaron Leopold <aaronleopold1221@gmail.com>
2020-10-10 20:30:08 -0400

    testing out ant design for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,13 @@
 import React from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
-  async function ping(e) {
-    e.preventDefault();
-
-    const res = await axios.get("/pingme").catch((error) => {
-      console.log(error.response);
-    });
-
-    if (res && res.data) {
-      alert(JSON.stringify(res.data));
-    } else {
-      alert("Error: maybe express server is not running?");
-    }
-  }
-
   return (
-    <div>
-      <h3>Woah, such empty</h3>
-
-      <div>
-        <label>just a little test</label>
-        <button onClick={ping}>ping the server</button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
