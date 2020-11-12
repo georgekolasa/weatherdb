ac2b9a0e5a66078f28b93271e44b6b2502af5554
Aaron Leopold <aaronleopold1221@gmail.com>
2020-11-11 21:01:09 -0500

    working demo for checkpoint

--- frontend/src/components/QueryBuilder/Toolbar.js
+++ frontend/src/components/QueryBuilder/Toolbar.js
@@ -1,8 +1,32 @@
-import { Button } from 'antd';
+import { Button, notification } from 'antd';
 import React from 'react';
+import axios from 'axios';
 import './styles/Toolbar.css';
 
 export default function ToolBar() {
+  function createNotification(description) {
+    notification.open({
+      message: 'Notification Title',
+      description,
+      duration: 0,
+    });
+  }
+
+  async function testQuery() {
+    const response = await axios
+      .post('/api/select')
+      .catch((error) => error.response);
+
+    if (response.status === 200) {
+      createNotification(JSON.stringify(response.data));
+    } else {
+      createNotification('Uh oh, something went wrong');
+    }
+
+    console.log(response);
+    // TODO: store in state
+  }
+
   return (
     <div className="toolbar-layout">
       <Button className="toolbar-btn" size="large">
@@ -11,8 +35,8 @@ export default function ToolBar() {
       <Button className="toolbar-btn" size="large">
         Visualization 2
       </Button>
-      <Button className="toolbar-btn" size="large">
-        Something Else
+      <Button className="toolbar-btn" size="large" onClick={testQuery}>
+        Test Select Query
       </Button>
 
       <Button className="toolbar-btn" size="large">
