60b07e07391cbf51090ca77f722422cf14e8f869
Aaron Leopold <aaronleopold1221@gmail.com>
2020-11-27 10:11:31 -0500

    download modal, chart loading states, updated stylings

--- frontend/src/components/PageLoader.js
+++ frontend/src/components/PageLoader.js
@@ -8,23 +8,9 @@ export default function PageLoader() {
   const loading = useStore((state) => state.loading);
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 0,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        pointerEvents: 'none',
-      }}
-    >
-      <div style={{ position: 'relative' }}>
-        <Spin size="large" spinning={loading} />
-      </div>
-    </div>
+    <CenteredDiv>
+      <Spin size="large" spinning={loading} />
+    </CenteredDiv>
   );
 }
 
--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -2,6 +2,12 @@ export const CHART_TYPES = {
   SCATTER: 'ScatterChart',
 };
 
+export const TREND_TYPES = {
+  LINEAR: {},
+  EXPONENTIAL: { type: 'exponential', visibleInLegend: true },
+  // POLYNOMIAL: {}, // kinda diff, bc you need a controlled degree field in the object
+};
+
 export const dbFields = [
   { label: 'LATITUDE', value: 'LATITUDE' },
   { label: 'LONGITUDE', value: 'LONGITUDE' },
