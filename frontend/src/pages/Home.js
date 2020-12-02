8e424ec65048578c2725b345101b9049ecabad84
Aaron Leopold <aaronleopold1221@gmail.com>
2020-12-02 09:51:51 -0500

    merging with dev, removing link to examples page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,19 +3,8 @@ import Header from '../components/Header/Header';
 import { useNavigate } from 'react-router-dom';
 import LazyHero from 'react-lazy-hero';
 import './Home.css';
-import { 
-  Button, 
-  Space, 
-  Typography,
-  Row, 
-  Col, 
-  Divider,
-  Card
-} from 'antd';
-import {
-  LineChartOutlined,
-  GlobalOutlined
-} from '@ant-design/icons'
+import { Button, Space, Typography, Row, Col, Divider, Card } from 'antd';
+import { LineChartOutlined, GlobalOutlined } from '@ant-design/icons';
 
 const style = { background: '#0092ff', padding: '8px 0' };
 
@@ -27,7 +16,7 @@ export default function Home() {
       <Header />
 
       {/* options: http://react-lazy-hero.danielstefanovic.com/ */}
-      
+
       {/* altervative hero pic options: https://s7d2.scene7.com/is/image/TWCNews/mike-bono-hurricane-season-08212019jpg?wid=2000&hei=1125&$wide-bg$
       
       https://s.hdnux.com/photos/75/45/75/16146241/3/1200x0.jpg
@@ -43,55 +32,86 @@ export default function Home() {
       >
         <h1 className="mainTitle">WeatherDB</h1>
         <Space direction="horizontal" size="middle">
-          <Button onClick={() => navigate('examples')}>
+          {/* <Button onClick={() => navigate('examples')}>
             See Examples Page
-          </Button>
-          <Button onClick={() => navigate('query')}>
-            See Query Builder Page
+          </Button> */}
+          <Button size="large" onClick={() => navigate('query')}>
+            Get Started
           </Button>
         </Space>
       </LazyHero>
-      
-      <Typography.Title className="subheader">What is WeatherDB?</Typography.Title>
-      
+
+      <Typography.Title className="subheader">
+        What is WeatherDB?
+      </Typography.Title>
+
       <div className="container">
-        <Card  
-          bordered={false} 
-          style={{width: 250}} 
-          cover={<img alt="world" src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/environmental_study_skau.svg"/>}>
-          <Typography.Text>See trends in global warming through a variety of climatological indicators.</Typography.Text>
-        </Card>
-        
-        
-        
-        <span className="vDivider"></span>
-        
-        <Card  
-          bordered={false} 
-          style={{width: 250}} 
-          cover={<img alt="world" src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/world_9iqb.svg"/>}>
-          <Typography.Text>Taken from thousands of datapoints and weather stations from across the globe.</Typography.Text>
+        <Card
+          bordered={false}
+          style={{ width: 250 }}
+          cover={
+            <img
+              alt="world"
+              src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/environmental_study_skau.svg"
+            />
+          }
+        >
+          <Typography.Text>
+            See trends in global warming through a variety of climatological
+            indicators.
+          </Typography.Text>
         </Card>
 
         <span className="vDivider"></span>
 
-        <Card  
-          bordered={false} 
-          style={{width: 250}} 
-          cover={<img alt="visualize-data" src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/setup_analytics_8qkl.svg"/>}>
-          <Typography.Text>Customize your visualization of the data with custom time ranges and custom parameters.</Typography.Text>
+        <Card
+          bordered={false}
+          style={{ width: 250 }}
+          cover={
+            <img
+              alt="world"
+              src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/world_9iqb.svg"
+            />
+          }
+        >
+          <Typography.Text>
+            Taken from thousands of datapoints and weather stations from across
+            the globe.
+          </Typography.Text>
         </Card>
 
-         {/* look at this: https://undraw.co/illustrations */}
+        <span className="vDivider"></span>
+
+        <Card
+          bordered={false}
+          style={{ width: 250 }}
+          cover={
+            <img
+              alt="visualize-data"
+              src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/setup_analytics_8qkl.svg"
+            />
+          }
+        >
+          <Typography.Text>
+            Customize your visualization of the data with custom time ranges and
+            custom parameters.
+          </Typography.Text>
+        </Card>
+
+        {/* look at this: https://undraw.co/illustrations */}
       </div>
-      
+
       <br></br>
-      
 
-      <h3><i>Big thanks to NOAA for providing the climatological data necessary for WeatherDB. The link to their data <a href="https://www.ncdc.noaa.gov/cdo-web/datasets">can be found here.</a></i></h3>
-          
-
-      
+      <h3>
+        <i>
+          Big thanks to NOAA for providing the climatological data necessary for
+          WeatherDB. The link to their data{' '}
+          <a href="https://www.ncdc.noaa.gov/cdo-web/datasets">
+            can be found here.
+          </a>
+        </i>
+      </h3>
     </div>
   );
 }
