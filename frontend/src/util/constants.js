1bcc7d02412ec3c30533930e0e32fd991637adff
Andrew Garmon <andrew.garmon@gmail.com>
2020-12-04 21:26:58 -0500

    Edited TREND1

--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,11 +1,25 @@
 export const trendQueries = {
-  TREND1: `SELECT EXTRACT(YEAR FROM DATE_TAKEN),
-  ROUND(AVG(VALUE),2) FROM GARMON.READING 
-  INNER JOIN GARMON.STATION USING(STATION_ID) 
-  WHERE ELEMENT='TAVG' AND COUNTRY='UK'
-  AND EXTRACT (YEAR FROM DATE_TAKEN) > 2000
-  GROUP BY EXTRACT (YEAR FROM DATE_TAKEN)
-  ORDER BY EXTRACT (YEAR FROM DATE_TAKEN)`,
+
+  TREND1: `SELECT * FROM 
+  (SELECT EXTRACT(YEAR FROM DATE_TAKEN) AS YEAR, ROUND(AVG(VALUE), 2) AS "Max Temp, 0.1 C"
+  FROM READING INNER JOIN STATION USING (STATION_ID)
+  WHERE ELEMENT='TMAX' AND STATE='FL'
+  GROUP BY EXTRACT(YEAR FROM DATE_TAKEN))
+  INNER JOIN
+  (SELECT EXTRACT(YEAR FROM DATE_TAKEN) AS YEAR, ROUND(AVG(VALUE), 2) AS "Min Temp, 0.1 C"
+  FROM READING INNER JOIN STATION USING (STATION_ID)
+  WHERE ELEMENT='TMIN' AND STATE='FL'
+  GROUP BY EXTRACT(YEAR FROM DATE_TAKEN))
+  USING (YEAR)
+  INNER JOIN
+  (SELECT EXTRACT(YEAR FROM DATE_TAKEN) AS YEAR, ROUND(AVG(VALUE), 2) AS "Avg total sunshine, minutes"
+  FROM READING INNER JOIN STATION USING (STATION_ID)
+  WHERE ELEMENT='TSUN' AND STATE='FL'
+  GROUP BY EXTRACT(YEAR FROM DATE_TAKEN))
+  USING (YEAR) 
+  WHERE YEAR > 1964
+  ORDER BY YEAR ASC`,
+
   TREND2: `SELECT country.name, avg(reading.value) "Temperature"
   FROM garmon.station 
   JOIN garmon.reading ON garmon.reading.station_id = garmon.station.station_id
@@ -14,11 +28,13 @@ export const trendQueries = {
   AND EXTRACT(YEAR from date_taken) <= '2020' AND EXTRACT(YEAR from date_taken) > '1996' 
   AND station.country = 'UK'
   GROUP BY country.name`, //FIXME
+
   TREND3: `SELECT avg(value) "Snow Depth", EXTRACT(YEAR from date_taken) "Year"
   FROM garmon.station JOIN garmon.reading ON garmon.reading.station_id = garmon.station.station_id
   WHERE element = 'SNWD' AND country = 'IC'
   GROUP BY EXTRACT(YEAR from date_taken)
   ORDER BY EXTRACT(YEAR from date_taken)`, //FIXME: flipped axis
+
   TREND4: `SELECT avg(value) "Temperature", EXTRACT(YEAR from date_taken) "Year"
   FROM garmon.station JOIN garmon.reading ON garmon.reading.station_id = garmon.station.station_id
   WHERE element = 'TAVG' 
@@ -26,6 +42,7 @@ export const trendQueries = {
   AND EXTRACT(YEAR from date_taken) <= '2020' AND EXTRACT(YEAR from date_taken) >= '2015' 
   GROUP BY EXTRACT(YEAR from date_taken)
   ORDER BY EXTRACT(YEAR from date_taken)`, //FIXME: flipped axis
+
   TREND5: `SELECT avg(value) "Wind Speed", EXTRACT(YEAR from date_taken) "Year"
   FROM garmon.station JOIN garmon.reading ON garmon.reading.station_id = garmon.station.station_id
   WHERE element = 'AWND'
@@ -33,25 +50,22 @@ export const trendQueries = {
   AND country = 'US'
   GROUP BY EXTRACT(YEAR from date_taken)
   ORDER BY EXTRACT(YEAR from date_taken)`,
-  TREND6: ``,
-  TEST_TREND: `SELECT EXTRACT(YEAR FROM DATE_TAKEN),
-  ROUND(AVG(VALUE),2) FROM GARMON.READING 
-  INNER JOIN GARMON.STATION USING(STATION_ID) 
-  WHERE ELEMENT='TAVG' AND COUNTRY='UK'
-  AND EXTRACT (YEAR FROM DATE_TAKEN) > 1974
-  GROUP BY EXTRACT (YEAR FROM DATE_TAKEN)
-  ORDER BY EXTRACT (YEAR FROM DATE_TAKEN)`,
+
+  TREND6: `SELECT EXTRACT(YEAR FROM DATE_TAKEN), ROUND(AVG(VALUE), 2)
+  FROM READING WHERE ELEMENT = 'TSUN' GROUP BY EXTRACT(YEAR FROM DATE_TAKEN) ORDER BY EXTRACT (YEAR FROM DATE_TAKEN) ASC`,
+  
+  TEST_TREND: ``,
 };
 
 export const chartConfigs = {
   TREND1: {
     chartType: 'ScatterChart',
     chartOptions: {
-      title: 'Average Daily Temperatures in the United Kingdom, 1990 - Present',
+      title: 'Average Daily Sunshine, Maximum and Minimum Daily Temperatures in Florida, 1965-present',
       hAxis: { format: '####', title: 'Year', minValue: 1990 },
       vAxis: { title: 'Temperature (0.1 C)' },
-      trendlines: { 0: { type: 'linear', color: 'red' } },
-      legend: 'none',
+      trendlines: { 0: { type: 'linear', color: 'blue' }, 1: {type: 'linear', color: 'red'}, 2: {type: 'linear', color: 'orange'} },
+      legend: {position: 'top'},
     },
   },
   TREND2: {
@@ -93,10 +107,9 @@ export const chartConfigs = {
   TREND6: {
     chartType: 'ScatterChart',
     chartOptions: {
-      title: 'Average ratio of snow fall to snow depth over X years',
-      vAxis: { format: '####', title: 'Year' },
-      hAxis: { title: 'Temperature (0.1 C)' },
-      trendlines: { 0: { type: 'linear', color: 'red' } },
+      title: 'Correlation between average temperature, maximum temperature, and snow depth',
+      hAxis: { title: 'Average Temperature' },
+      trendlines: { 0: { type: 'linear', color: 'blue' }, 1: {type: 'linear', color: 'red'}},
     },
   },
   TEST_TREND: {
@@ -112,7 +125,7 @@ export const chartConfigs = {
 };
 
 export const trendNames = [
-  { label: 'Trend 1 name', value: 'TREND1' },
+  { label: 'Florida average temperatures and sunshine', value: 'TREND1' },
   { label: 'Trend 2 name', value: 'TREND2' },
   { label: 'Trend 3 name', value: 'TREND3' },
   { label: 'Trend 4 name', value: 'TREND4' },
@@ -125,7 +138,11 @@ export const trendNames = [
 ];
 
 export const highlights = {
-  TREND1: [],
+  TREND1: [
+    "Since 1965, Florida's daily highs and lows have been getting warmer.",
+    "Since 1965, Florida is experiencing less sunshine per day, on average.",
+    "This data was analyzed from 9,143,924 readings taken at 1,744 Florida weather stations."
+  ],
   TREND2: [],
   TREND3: [],
   TREND4: [],
